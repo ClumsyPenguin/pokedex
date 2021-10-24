a7f9099b12f20c4ab7f7fa086da82714527e2a4c
clumpsypenguin <geerinckxseppe@gmail.com>
2021-10-24 14:57:15 +0100

    init proje

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1></h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import PokemonClass from "../models/Pokemon.model";
+
+@Options({
+  props: {
+    pokemon: {
+      type: Object as () => PokemonClass,
+      required: true
+    }
+  },
+})
+export default class PokemonInfo extends Vue {
+  
+}
+</script>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <p>Pokemon</p>
+    <Pokemon :pokemon="pokemon" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Pokemon from "../components/Pokemon.vue"
+
+@Options({
+  components: {
+    Pokemon
+  },
+})
+export default class Home extends Vue {
+  private pokemon : Pokemon = new Pokemon();
+  mounted() {
+    
+  }
+}
+</script>
